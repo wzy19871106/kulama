8459fff41fdcbfaf8b239ea49972ea52c9ac6672
zmm <zmm@smartrace.cn>
2019-05-27 15:29:51 +0800

    上传修改

--- shianxian-supervise/src/main/java/cn/shianxian/supervise/sys/controller/UploadController.java
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/sys/controller/UploadController.java
@@ -50,39 +50,33 @@ public class UploadController {
      */
     @PostMapping("img")
     @ApiOperation(value = "服务器通用上传接口", notes = "上传到服务器")
-    @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "query", name = "key", value = "文件名，可传可不传，如果文件名在服务器存在会删除该文件，然后存储。请带上后缀名。"),
-            @ApiImplicitParam(paramType = "query", name = "path", value = "文件路径", required = true),
-    })
-    public ResponseEntity<Result> uploadImg(@ApiParam(value = "要上传的文件", required = true) MultipartFile file, @RequestParam(value = "key", required = false) String key, @RequestParam(value = "path", required = false) String path) throws IOException {
+    @ApiImplicitParam(paramType = "query", name = "key", value = "文件名，可传可不传，如果文件名在服务器存在会删除该文件，然后存储。请带上后缀名。")
+    public ResponseEntity<Result> uploadImg(@ApiParam(value = "要上传的文件", required = true) MultipartFile file, @RequestParam(value = "key", required = false) String key) throws IOException {
         String fileSuffix = CommonUtils.getFileSuffix(file.getOriginalFilename());
         if (!fileType.contains(fileSuffix)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Result.msg("请上传图片！"));
-        }
-        if (path.indexOf("/") < 0) {
-            path = path + "/";
         }
         String fileName = UUIDGenerator.generatorUUID();
         // 如果是修改图片，先把之前的图片删除
         if (StringUtils.isNotEmpty(key)) {
             // 删除临时文件
-            File oldFile = new File( uploadPath + path + key);
+            File oldFile = new File( uploadPath + key);
             if (oldFile.exists()) {
                 oldFile.delete();
             }
             fileName = key.substring(0, key.lastIndexOf("."));
         }
-        File f = new File(uploadPath + path);
+        File f = new File(uploadPath);
         if (!f.exists()) {
             f.mkdirs();
-            log.info("创建文件夹：{}" + path);
+            log.info("创建文件夹：{}" + uploadPath);
         }
         fileName = fileName + fileSuffix;
-        String filepath = uploadPath + path + fileName;
+        String filepath = uploadPath + fileName;
         File tmpFile = new File(filepath);
         file.transferTo(tmpFile);
         log.info("上传文件成功：{}" + fileName);
-        return ResponseEntity.ok(Result.data(uploadUrl + path + fileName));
+        return ResponseEntity.ok(Result.data(uploadUrl + fileName));
     }
 
 }
