f5306176d5ecb8cdd7ac74982abc332fe6f44dbc
zmm <zmm@smartrace.cn>
2019-06-03 19:52:14 +0800

    政企互动完成

---
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/information/controller/InformationController.java
@@ -0,0 +1,113 @@
+package cn.shianxian.supervise.information.controller;
+
+import cn.shianxian.supervise.common.pojo.Pages;
+import cn.shianxian.supervise.common.pojo.QueryPojo;
+import cn.shianxian.supervise.common.pojo.Result;
+import cn.shianxian.supervise.information.pojo.Information;
+import cn.shianxian.supervise.information.service.InformationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * 政企互动控制器
+ */
+@RestController
+@RequestMapping("information")
+@Api(description = "政企互动控制器")
+@Slf4j
+public class InformationController {
+
+
+    @Autowired
+    private InformationService informationService;
+
+
+    /**
+     * 保存、修改政企互动信息
+     * @return
+     */
+    @PostMapping("information")
+    @ApiOperation(value = "保存、修改政企互动信息", notes = "保存、修改政企互动信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "index", value = "政企互动标识"),
+            @ApiImplicitParam(paramType = "query", name = "title", value = "政企互动标题"),
+            @ApiImplicitParam(paramType = "query", name = "picTag", value = "政企互动图片"),
+            @ApiImplicitParam(paramType = "query", name = "content", value = "政企互动内容"),
+            @ApiImplicitParam(paramType = "query", name = "ifTop", value = "是否置顶"),
+            @ApiImplicitParam(paramType = "query", name = "ifDisable", value = "是否启用"),
+            @ApiImplicitParam(paramType = "query", name = "createTime", value = "创建时间"),
+            @ApiImplicitParam(paramType = "query", name = "updateTime", value = "最后修改时间"),
+            @ApiImplicitParam(paramType = "query", name = "columnTag", value = "栏目标识"),
+            @ApiImplicitParam(paramType = "query", name = "userId", value = "对象用户标识(暂不用)"),
+            @ApiImplicitParam(paramType = "query", name = "userGroupId", value = "对象用户组标识(暂不用)"),
+    })
+    public ResponseEntity<Result> saveInformation(@Valid Information information) {
+        log.info("保存、修改政企互动信息：{}", information);
+        return this.informationService.saveInformation(information);
+    }
+
+
+    /**
+     * 删除政企互动信息
+     * @return
+     */
+    @DeleteMapping("information")
+    @ApiOperation(value = "删除政企互动信息", notes = "删除政企互动信息")
+    @ApiImplicitParam(paramType = "query", name = "index", value = "政企互动标识")
+    public ResponseEntity<Result> deleteInformation(@RequestParam Long index) {
+        log.info("删除政企互动信息：{}", index);
+        return this.informationService.deleteInformation(index);
+    }
+
+
+    /**
+     * 查询政企互动信息
+     * @return
+     */
+    @GetMapping("information")
+    @ApiOperation(value = "查询政企互动信息", notes = "查询政企互动信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "id", value = "栏目id"),
+            @ApiImplicitParam(paramType = "query", name = "name", value = "关键字"),
+            @ApiImplicitParam(paramType = "query", name = "pageNum", value = "第几页"),
+            @ApiImplicitParam(paramType = "query", name = "pageSize", value = "每页查询数量"),
+    })
+    public ResponseEntity<Result> selectInformation(QueryPojo queryPojo, Pages pages) {
+        return this.informationService.selectInformation(queryPojo, pages);
+    }
+
+
+    /**
+     * 根据id查询政企互动信息
+     * @return
+     */
+    @GetMapping("selectInformationById")
+    @ApiOperation(value = "根据id查询政企互动信息", notes = "根据id查询政企互动信息")
+    @ApiImplicitParam(paramType = "query", name = "index", value = "政企互动标识")
+    public ResponseEntity<Result> selectInformationById(String index) {
+        return this.informationService.selectInformationById(index);
+    }
+
+
+    /**
+     * 修改政企互动信息置顶
+     * @return
+     */
+    @PutMapping("updateInformationTop")
+    @ApiOperation(value = "修改政企互动信息置顶", notes = "修改政企互动信息置顶")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "index", value = "政企互动标识"),
+            @ApiImplicitParam(paramType = "query", name = "type", value = "类型[1：置顶，2：取消置顶]"),
+    })    public ResponseEntity<Result> updateInformationTop(String index, int type) {
+        return this.informationService.updateInformationTop(index, type);
+    }
+
+}
---
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/information/pojo/Information.java
@@ -0,0 +1,93 @@
+package cn.shianxian.supervise.information.pojo;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+/**
+ * 政企互动信息表
+ */
+@Table(name = "t_information")
+@Data
+public class Information {
+
+
+    /**
+     * 政企互动标识
+     */
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    @Column(name = "`index`")
+    private Long index;
+
+    /**
+     * 政企互动标题
+     */
+    @Column(name = "`title`")
+    @NotEmpty(message = "标题不能为空！")
+    private String title;
+
+    /**
+     * 政企互动图片
+     */
+    @Column(name = "`picTag`")
+    private String picTag;
+
+    /**
+     * 政企互动内容
+     */
+    @Column(name = "`content`")
+    @NotEmpty(message = "内容不能为空！")
+    private String content;
+
+    /**
+     * 是否置顶
+     */
+    @Column(name = "`ifTop`")
+    private String ifTop;
+
+    /**
+     * 是否启用
+     */
+    @Column(name = "`ifDisable`")
+    private String ifDisable;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "`createTime`")
+    private LocalDateTime createTime;
+
+    /**
+     * 最后修改时间
+     */
+    @Column(name = "`updateTime`")
+    private LocalDateTime updateTime;
+
+    /**
+     * 栏目标识
+     */
+    @Column(name = "`columnTag`")
+    private String columnTag;
+
+    /**
+     * 对象用户标识(暂不用)
+     */
+    @Column(name = "`userId`")
+    private String userId;
+
+    /**
+     * 对象用户组标识(暂不用)
+     */
+    @Column(name = "`userGroupId`")
+    private String userGroupId;
+
+    /**
+     * 栏目名称
+     */
+    @Transient
+    private String columnName;
+}
