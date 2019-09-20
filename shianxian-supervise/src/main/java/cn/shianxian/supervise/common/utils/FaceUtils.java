168b853d7c6847feb4cdb3a3fc6ed4f69dcae672
jww <jww@honest1101>
2019-09-20 10:48:06 +0800

    未检测到人脸 处理

--- shianxian-supervise/src/main/java/cn/shianxian/supervise/common/utils/FaceUtils.java
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/common/utils/FaceUtils.java
@@ -64,8 +64,7 @@ public class FaceUtils {
 
         if (faceInfoList.isEmpty() || faceInfoList2.isEmpty()) {
             log.info("没有人脸信息");
-            map.put("flag",false);
-            map.put("status",404);
+            map.put("message","没有检测到人脸");
             return map;
         }
         // 特征提取
