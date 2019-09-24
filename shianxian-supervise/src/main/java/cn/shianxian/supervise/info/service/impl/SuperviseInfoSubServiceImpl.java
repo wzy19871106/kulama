dccabaaf7f0436ef5e3175688f5d33215b9807a8
jww <jww@honest1101>
2019-09-24 17:50:09 +0800

    截取 反馈

--- shianxian-supervise/src/main/java/cn/shianxian/supervise/info/service/impl/SuperviseInfoSubServiceImpl.java
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/info/service/impl/SuperviseInfoSubServiceImpl.java
@@ -47,17 +47,15 @@ public class SuperviseInfoSubServiceImpl implements SuperviseInfoSubService {
         String picTag = infoSub.getPicTag();
         // 去掉 , 号
         String[] splits = picTag.split(",");
+        StringBuilder sb = new StringBuilder();
         if (splits != null && splits.length > 0) {
-            StringBuilder sb = new StringBuilder();
-            if (splits.length > 0) {
-                for (String split : splits) {
-                    // 判断是否是反馈
-                    if (split.indexOf("反馈") != -1) {
-                        // 截掉前三个字符  反馈:
-                        String substring = split.substring(3);
-                        String sub = substring + ",";
-                        sb.append(sub);
-                    }
+            for (String split : splits) {
+                // 判断是否是反馈
+                if (split.indexOf("反馈") != -1) {
+                    // 截掉前三个字符  反馈:
+                    String substring = split.substring(3);
+                    String sub = substring + ",";
+                    sb.append(sub);
                 }
             }
             infoSub.setPicTag(sb.toString());
