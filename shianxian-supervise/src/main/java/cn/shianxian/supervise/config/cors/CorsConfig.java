9798354541edcb17f6140fc9d06ce00dbb773a62
jww <jww@honest1101>
2019-10-12 16:05:33 +0800

    跨域

--- shianxian-supervise/src/main/java/cn/shianxian/supervise/config/cors/CorsConfig.java
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/config/cors/CorsConfig.java
@@ -28,9 +28,9 @@ public class CorsConfig {
         list.add("http://127.0.0.1:8083");
         list.add("https://127.0.0.1:8083");
         list.add("https://supervise.dev.shianxian.cn");
-        list.add("http://supervise.dev.shianxian.cn");
+//        list.add("http://supervise.dev.shianxian.cn");
         list.add("https://supervise.test.shianxian.cn");
-        list.add("http://supervise.test.shianxian.cn");
+//        list.add("http://supervise.test.shianxian.cn");
         config.setAllowedOrigins(list);
         // 允许访问的头信息,*表示全部
         config.addAllowedHeader("*");
