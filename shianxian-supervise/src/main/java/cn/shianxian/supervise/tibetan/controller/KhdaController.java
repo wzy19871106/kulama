82c3b50fe8ca9d1d33721c525d3b1775108c1249
jww <jww@honest1101>
2019-10-22 09:58:10 +0800

    post

--- shianxian-supervise/src/main/java/cn/shianxian/supervise/tibetan/controller/KhdaController.java
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/tibetan/controller/KhdaController.java
@@ -9,7 +9,7 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class KhdaController {
     @Autowired
     private KhdaService khdaService;
 
-    @GetMapping("selectCustomerInfoByNameAndPass")
+    @PostMapping("selectCustomerInfoByNameAndPass")
     @ApiOperation(value = "根据用户名和密码查询客户信息",notes = "根据用户名和密码查询客户信息")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType = "query",name = "khloginname",value = "登录用户名"),
