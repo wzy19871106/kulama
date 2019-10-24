689f50261cfaeb71a86fbaa9ced9e749dd5aa357
jww <jww@honest1101>
2019-10-24 09:24:16 +0800

    语音

--- shianxian-supervise/src/main/java/cn/shianxian/supervise/push/controller/PushController.java
+++ shianxian-supervise/src/main/java/cn/shianxian/supervise/push/controller/PushController.java
@@ -1,18 +1,26 @@
 package cn.shianxian.supervise.push.controller;
 
+import cn.shianxian.supervise.common.constants.Constants;
 import cn.shianxian.supervise.common.pojo.Result;
 import cn.shianxian.supervise.common.utils.JpushUtil;
 import cn.shianxian.supervise.push.vo.JpushVO;
+import com.submail.config.AppConfig;
+import com.submail.lib.VoiceXSend;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +33,8 @@ import java.util.Map;
 @Slf4j
 public class PushController {
 
+    private static final String APP_ID = "21295";
+    private static final String APP_KEY = "57e964bb738cdb99cb8d1d9fc229b8c2";
     /**
      * 极光推送
      */
@@ -36,14 +46,43 @@ public class PushController {
             @ApiImplicitParam(paramType = "query", name = "alias", value = "别名"),
             @ApiImplicitParam(paramType = "query", name = "id", value = "registrationId指定用户")
     })
-    public ResponseEntity<Result> JpushAndroid(JpushVO jpushVO){
+    public ResponseEntity<Result> JpushAndroid(JpushVO jpushVO) {
         Map<String, String> parm = new HashMap<>();
-        parm.put("msg",jpushVO.getMsg());
-        parm.put("title",jpushVO.getTitle());
-        parm.put("alias",jpushVO.getAlias());
-        parm.put("id",jpushVO.getId());
+        parm.put("msg", jpushVO.getMsg());
+        parm.put("title", jpushVO.getTitle());
+        parm.put("alias", jpushVO.getAlias());
+        parm.put("id", jpushVO.getId());
         Result result = JpushUtil.jpushAndroid(parm);
-        log.info("极光推送，{}",result);
+        log.info("极光推送，{}", result);
         return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 语音消息
+     *
+     * @return
+     */
+    @PostMapping("xsendConfig")
+    @ApiOperation(value = "语音消息", notes = "语音消息")
+    public Result xsendConfig(String userTal) {
+//        AppConfig config = ConfigLoader.load(ConfigLoader.ConfigType.Voice);
+        AppConfig appConfig = new AppConfig();
+        appConfig.setAppId(APP_ID);
+        appConfig.setAppKey(APP_KEY);
+        VoiceXSend submail = new VoiceXSend(appConfig);
+        submail.addTo(userTal);
+        submail.addProject("XebWP");
+//        submail.addVars("name","张三");
+//        submail.addVars("code","2244");
+        String xsend = null;
+        try {
+            xsend = submail.xsend();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.info("语音：{}", xsend);
+        return Result.data(xsend);
     }
 }
